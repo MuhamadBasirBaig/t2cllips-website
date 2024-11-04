02a210a05cf33a999c4d6adb22eb6572d4771bcf
Muhammad Basir Baig <basir.baig81@gmail.com>
2024-11-05 03:45:15 +0500

    updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 <script>
   (function() {
     // Check if the current URL is a GitHub Pages URL
-    if (window.location.hostname === "muhamadbasirbaig.github.io/") {
+    if (window.location.hostname === "muhamadbasirbaig.github.io") {
       var base = document.createElement('base');
       base.href = '/t2cllips-website/';
       document.head.prepend(base);
