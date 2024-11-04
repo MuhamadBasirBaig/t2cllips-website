e73a08bfac2d6502aa5bc2e7d062dddb53f32e0f
Muhammad Basir Baig <basir.baig81@gmail.com>
2024-11-05 03:33:40 +0500

    added scrip.js and include in index.html

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+  (function() {
+    // Check if the current URL is a GitHub Pages URL
+    if (window.location.hostname === "muhamadbasirbaig.github.io/") {
+      var base = document.createElement('base');
+      base.href = '/t2cllips-website/';
+      document.head.prepend(base);
+    }
+  })();
+</script>
